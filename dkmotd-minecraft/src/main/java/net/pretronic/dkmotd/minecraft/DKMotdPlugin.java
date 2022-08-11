fa309d2ab0682fb017cf3b777a4f13ed167dd08f
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2022-08-11 19:25:46 +0200

    Update DKMotdPlugin.java

--- dkmotd-minecraft/src/main/java/net/pretronic/dkmotd/minecraft/DKMotdPlugin.java
+++ dkmotd-minecraft/src/main/java/net/pretronic/dkmotd/minecraft/DKMotdPlugin.java
@@ -19,27 +19,12 @@ import org.mcnative.runtime.api.player.tablist.Tablist;
 import org.mcnative.runtime.api.plugin.MinecraftPlugin;
 
 public class DKMotdPlugin extends MinecraftPlugin {
-
-    public static String RESOURCE_ID ="9304d68a-bbdf-11eb-8ba0-0242ac180002";
-    public static String PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkzlaMKAoRUdoFOnCbd+ehZXQN4etqIWkalsGZjbKkdMnxpwhInWPtKptWqOZBa7/r8y/vZ0E0KjFXwj/dVDpjb5iCfAKw/keHsAQDF4I0vAknKtSajQjpO1DL4jFD8yaXnfbu3I4J1xbSsUgybUyvR+Mv6zyouJ4rIQd84Njd1+fRdLZnblmeFHXKjNyizSfyrJlgYOECmxccCjWMoJjZpk2CBNwqyCQOqB1/t8WZchPC0ZpXyLyY2PW3coB1YgmUqrve6t+Eyts6vsMTijRXBaHohKrXhmTYjqz/Bc4OeiggL2nPNbvDI99nIUu+2IY42AleZQu08HcbQ0TS5EZ5wIDAQAB";
-
+    
     private DefaultDKMotd dkMotd;
 
     @Lifecycle(state = LifecycleState.LOAD)
     public void onLoad(LifecycleState state){
         getLogger().info("DKMotd is starting, please wait..");
-
-        try{
-            McNativeLicenseIntegration.newContext(this,RESOURCE_ID,PUBLIC_KEY).verifyOrCheckout();
-        }catch (LicenseNotValidException | CloudNotCheckoutLicenseException e){
-            getLogger().error("--------------------------------");
-            getLogger().error("-> Invalid license");
-            getLogger().error("-> Error: "+e.getMessage());
-            getLogger().error("--------------------------------");
-            getLogger().info("DKMotd is shutting down");
-            getLoader().shutdown();
-            return;
-        }
 
         getConfiguration().load(DKMotdConfig.class);
 
